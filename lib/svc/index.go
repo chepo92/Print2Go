72b8112cb0738d00af317bde286809627e575e60
Adrian Ulrich <adrian@blinkenlights.ch>
2020-09-27 14:33:34 +0200

    bump js stuff

--- lib/svc/index.go
+++ lib/svc/index.go
@@ -11,10 +11,13 @@ func (svc *Svc) indexPage(w http.ResponseWriter) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <script src="https://cdn.jsdelivr.net/npm/vue@2.6.11"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
         <title>gfeeder status</title>
     </head>
     <body>
@@ -22,14 +25,15 @@ func (svc *Svc) indexPage(w http.ResponseWriter) {
 <br>
 
 <template v-if="jobRunning">
+
   <h3>{{ jobDescription }}</h3>
-  <button v-on:click="onCancelJob">Cancel current job</button>
+  <button v-on:click="onCancelJob" class="btn btn-danger">Cancel current job</button>
 </template>
 <template v-else>
   <div class="file-upload">
     <input type="file" @change="onGcodeFileSelected" />
     <br>
-    <button @click="onGcodeStartUpload" class="upload-button" :disabled="!this.selectedFile">Print file</button>
+    <button @click="onGcodeStartUpload" class="btn btn-primary" :disabled="!this.selectedFile">Print file</button>
   </div>
 </template>
 
@@ -45,7 +49,7 @@ new Vue({
   },
   methods: {
     loadData: function () {
-      $.get('api/job', function (response) {
+      jQuery.get('api/job', function (response) {
         this.jobRunning = response.job != null;
         this.jobDescription = response.status;
       }.bind(this));
@@ -85,7 +89,6 @@ new Vue({
             alert('post failure: ' + data.responseText);
         },
       });
-
     },
   },
   mounted: function () {
