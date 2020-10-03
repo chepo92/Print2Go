0f7ae17e764ae3ca2c8e4575afd3ac24bffc098c
Adrian Ulrich <adrian@blinkenlights.ch>
2020-10-03 19:40:37 +0200

    Add comet connection to job api

--- lib/svc/job.go
+++ lib/svc/job.go
@@ -1,25 +1,50 @@
 package svc
 
 import (
+	"fmt"
+	"hash/fnv"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type jobStatus struct {
+	File   string  `json:"file"`
+	Done   float64 `json:"donePct"`
+	Desc   string  `json:"description"`
+	Cmd    string  `json:"lastcmd"`
+	Reply  string  `json:"lastreply"`
+	Active bool    `json:"active"`
+	Hash   uint32  `json:"fp"`
 }
 
 func (svc *Svc) jobStatus(w http.ResponseWriter, rq *http.Request) {
-	svc.RLock()
-	defer svc.RUnlock()
+	nid, _ := strconv.Atoi(rq.FormValue("id"))
+	id := uint32(nid)
 
-	if rq.Method == "GET" {
-		svc.error(w, "Fixme")
-		return
-	}
-	if rq.Method == "POST" && svc.task != nil {
-		if rq.FormValue("cancel") == "true" {
-			svc.task.Cancel()
-			return
+	c := svc.task.Subscribe()
+	defer svc.task.Unsubscribe(c)
+
+	var js jobStatus
+	for range []int{1, 2} {
+		select {
+		case <-time.After(time.Second * 20):
+		case v := <-c:
+			js = jobStatus{
+				File:   v.File,
+				Done:   v.Done,
+				Desc:   v.Text,
+				Cmd:    v.LastCommand,
+				Reply:  v.LastReply,
+				Active: v.Active,
+			}
+			h := fnv.New32a()
+			h.Write([]byte(fmt.Sprintf("%+v", js)))
+			js.Hash = h.Sum32() & 0xEFFF
+		}
+		if js.Hash != id {
+			break
 		}
 	}
-	svc.error(w, "bad request to job status endpoint")
+	jsonWrite(w, js)
 }
