ec4a5adb57173bbb172a1651cb81b2f1e21f32d3
Adrian Ulrich <adrian@blinkenlights.ch>
2020-10-03 18:38:15 +0200

    add virtual test printer

---
+++ util/vprinter.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+var (
+	flagTTY = flag.String("tty", "/tmp/virtual-tty", "path to tty")
+)
+
+func main() {
+	flag.Parse()
+
+	fh, err := os.OpenFile(*flagTTY, os.O_RDWR, 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	fh.Write([]byte("printer booting blabla\nok\n"))
+	s := bufio.NewScanner(fh)
+	for s.Scan() {
+		fmt.Printf(">> %s\n", s.Text())
+		time.Sleep(time.Second)
+		fh.Write([]byte("ok\n"))
+	}
+}
