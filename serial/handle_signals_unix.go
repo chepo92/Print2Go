d57b59d935d95e31a6680585f03a19956a81cde6
Axel Sepulveda <ansepulveda@uc.cl>
2025-09-05 23:26:43 -0400

    fix duplicated func declaration

--- serial/handle_signals_unix.go
+++ serial/handle_signals_unix.go
@@ -1,24 +1,24 @@
+//go:build !windows
 // +build !windows
 
 package serial
 
 import (
-    "os"
-    "syscall"
-    "fmt"
-    "io"
-    "os/signal"
+	"fmt"
+	"io"
+	"os"
+	"syscall"
 )
 
 // Escape hatch for stalled prints: signals can be used to send a command to the printer or pretend that we received an OK.
 func handleSignals(s <-chan os.Signal, w io.Writer) {
-    for sig := range s {
-        switch sig {
-        case syscall.SIGUSR1:
-            // send 'ok' back to takoprint.
-            fmt.Fprintf(os.Stdout, "ok\r\n")
-        default:
-            // unhandled.
-        }
-    }
+	for sig := range s {
+		switch sig {
+		case syscall.SIGUSR1:
+			// send 'ok' back to takoprint.
+			fmt.Fprintf(os.Stdout, "ok\r\n")
+		default:
+			// unhandled.
+		}
+	}
 }
--- serial/signals_unix.go
+++ serial/signals_unix.go
@@ -1,26 +1,26 @@
+//go:build !windows
 // +build !windows
 
 package serial
 
 import (
-    "os"
-    "os/signal"
-    "syscall"
-    "io"
-    "fmt"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 // Solo en Unix: inicializa señales y maneja SIGUSR1
 func setupSignals(w io.Writer) {
-    sigs := make(chan os.Signal)
-    signal.Notify(sigs, syscall.SIGUSR1)
-    go handleSignals(sigs, w)
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, syscall.SIGUSR1)
+	go handleSignals(sigs, w)
 }
 
-func handleSignals(sigs <-chan os.Signal, w io.Writer) {
-    for sig := range sigs {
-        if sig == syscall.SIGUSR1 {
-            fmt.Fprintf(w, "ok\r\n")
-        }
-    }
-}
+// func handleSignals(sigs <-chan os.Signal, w io.Writer) {
+//     for sig := range sigs {
+//         if sig == syscall.SIGUSR1 {
+//             fmt.Fprintf(w, "ok\r\n")
+//         }
+//     }
+// }
