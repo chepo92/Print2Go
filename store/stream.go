e6f97c991e1f9f6f122deb4d777199c61d677ac3
Axel Sepulveda <ansepulveda@uc.cl>
2025-09-08 18:40:16 -0300

    add comments

--- store/stream.go
+++ store/stream.go
@@ -4,9 +4,14 @@ import (
 	"io"
 )
 
+// Stream is an interface for a gcode stream
 type Stream interface {
+	// underlying reader
 	io.ReadCloser
+	// total size of the stream
 	Size() int64
+	// current read position
 	Pos() int64
+	// name of the stream, usually the filename?
 	Name() string
 }
