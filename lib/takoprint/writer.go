9fe8ea62c300713360b9090377d80548b919a827
Axel Sepulveda <ansepulveda@uc.cl>
2025-09-08 13:18:59 -0300

    improve writer

--- lib/takoprint/writer.go
+++ lib/takoprint/writer.go
@@ -2,28 +2,62 @@ package takoprint
 
 import (
 	"context"
+	"time"
 )
 
 // feedPrinter writes a new line to the printer after receiving an okChan interrupt.
 // calls the passed in cancel func once all data was consumed.
 func (tp *Takoprint) feedPrinter(ctx context.Context, cancel context.CancelFunc, okChan <-chan bool) {
 	defer cancel()
+	// start := time.Now()
+	// inLoop := false
 	for {
 		select {
 		case <-ctx.Done():
 			return
-		case _, ok := <-okChan:
-			if !ok {
-				// ok chan closed.
+		case okValue, okChanStatus := <-okChan:
+			// inLoop = false
+			if !okChanStatus {
+				// ok channel is closed, return
+				tp.log.Printf("ok channel is closed, returning")
 				return
+			} // else, we can send more data.
+			if !okValue {
+				// printer sent something other than "ok", do not send more data now.
+				tp.log.Printf("Printer busy, waiting and skipping...")
+				time.Sleep(1000 * time.Millisecond) // pausa de 1000ms
+
+			} else {
+				tp.log.Printf("Ok received in channel, reading next line to be sent")
+				// get next line from feedIn channel
+				next, ok := <-tp.feedIn
+				if !ok {
+					// feed is empty, no more data to send!
+					tp.log.Printf("Feed is empty, no more data to send!")
+					return
+				}
+				// send next line to printer
+				tp.log.Printf("Sending next line to printer: %q", next)
+				tp.sendCommand(next)
 			}
-			next, ok := <-tp.feedIn
-			if !ok {
-				// feed is empty, no more data to send!
-				return
-			}
-			tp.sendCommand(next)
+			// default:
+			// 	if !inLoop {
+			// 		inLoop = true
+			// 		tp.log.Println("No se recibió ninguna señal en writer, iniciando timeout")
+			// 		start = time.Now()
+			// 	} else {
+			// 		tp.log.Println("No se recibió ninguna señal en writer, esperando todavia, timeout activado")
+			// 		time.Sleep(1000 * time.Millisecond) // pausa de 1000ms
+			// 		// ...
+			// 	}
+
 		}
+
+		// // Agregar una condición para que el programa termine después de un cierto período de tiempo in case is in a loop
+		// if inLoop && (time.Since(start) > 20*time.Second) {
+		// 	tp.log.Println("Se alcanzó el tiempo máximo de ejecución para writer")
+		// 	return
+		// }
 	}
 }
 
@@ -33,7 +67,7 @@ func (tp *Takoprint) sendCommand(l string) {
 	tp.stats.numSent++
 	tp.stats.lastCmd = l
 	tp.Unlock()
-
+	tp.log.Println("Sending: ", l)
 	tp.serialOut.Write(append([]byte(l), '\r', '\n'))
 }
 
