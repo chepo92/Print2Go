3dd96b630e80fafde678d36284a7dae11c4774de
Axel Sepulveda <ansepulveda@uc.cl>
2025-09-08 13:09:16 -0300

    rename var

--- task/task.go
+++ task/task.go
@@ -50,111 +50,112 @@ func New() *Task {
 	}
 }
 
-func (t *Task) Launch(p io.ReadWriteCloser, gcs store.Stream) error {
-	t.Lock()
-	defer t.Unlock()
+func (task *Task) Launch(p io.ReadWriteCloser, gcs store.Stream) error {
+	task.Lock()
+	defer task.Unlock()
 
-	if t.gcodeStream != nil {
-		return fmt.Errorf("task already running")
+	if task.gcodeStream != nil {
+		return fmt.Errorf("Task already running")
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
-	t.tp = takoprint.New(p, gcs)
-	t.ctx = ctx
-	t.cancel = cancel
-	t.gcodeStream = gcs
-	t.status.Done = 0
-	t.status.Active = true
-	t.status.Text = "starting..."
-	t.status.Started = time.Now()
+	task.tp = takoprint.New(p, gcs)
+	task.ctx = ctx
+	task.cancel = cancel
+	task.gcodeStream = gcs
+	task.status.Done = 0
+	task.status.Active = true
+	task.status.Text = "starting..."
+	task.status.Started = time.Now()
 
 	// horray for circular dependencies!
-	takoprint.Callback(t.callback)(t.tp)
+	takoprint.Callback(task.callback)(task.tp)
 
 	// semi-empty callback to anounce that we are now active.
-	go t.callback(&takoprint.CallbackData{})
-	go t.start()
+	go task.callback(&takoprint.CallbackData{})
+	go task.start()
 	return nil
 }
 
 // Returns true if the task is done.
-func (t *Task) Done() bool {
-	t.RLock()
-	defer t.RUnlock()
-	return t.ctx == nil || t.ctx.Err() != nil
+func (task *Task) Done() bool {
+	task.RLock()
+	defer task.RUnlock()
+	return task.ctx == nil || task.ctx.Err() != nil
 }
 
-func (t *Task) WaitDone() <-chan struct{} {
-	t.RLock()
-	defer t.RUnlock()
+func (task *Task) WaitDone() <-chan struct{} {
+	task.RLock()
+	defer task.RUnlock()
 
-	if t.ctx == nil {
+	if task.ctx == nil {
 		return nil
 	}
-	return t.ctx.Done()
+	return task.ctx.Done()
 }
 
 // Cancel calls the context cancel function, aborting the task.
-func (t *Task) Cancel() {
-	t.RLock()
-	defer t.RUnlock()
-	if t.cancel == nil {
+func (task *Task) Cancel() {
+	task.RLock()
+	defer task.RUnlock()
+	if task.cancel == nil {
 		return
 	}
-	t.cancel()
+	task.cancel()
 }
 
 // start internally launches the task and sets 'done' once the print finished.
-func (t *Task) start() {
-	t.tp.Start(t.ctx)
+func (task *Task) start() {
+	task.tp.Start(task.ctx)
 	// mark own context as done.
 
 	// cancel our contex and fire an empty callback
-	t.Cancel()
-	t.callback(nil)
-	t.nullify()
+	task.Cancel()
+	task.callback(nil)
+	task.nullify()
 }
 
-func (t *Task) nullify() {
-	t.Lock()
-	defer t.Unlock()
+// nullify clears most of the struct, allowing for a new task to be started.
+func (task *Task) nullify() {
+	task.Lock()
+	defer task.Unlock()
 
 	// nulls *most* of the struct.
-	t.tp = nil
-	t.ctx = nil
-	t.cancel = nil
-	t.gcodeStream = nil
+	task.tp = nil
+	task.ctx = nil
+	task.cancel = nil
+	task.gcodeStream = nil
 }
 
 // called by takoprint to update the status.
-func (t *Task) callback(d *takoprint.CallbackData) {
+func (task *Task) callback(d *takoprint.CallbackData) {
 	var txt string
 
-	t.Lock()
+	task.Lock()
 	// notify subscribers (must happen after unlock popped from stack)
-	defer t.broadcast()
-	defer t.Unlock()
+	defer task.broadcast()
+	defer task.Unlock()
 	defer func() {
-		if txt != t.status.Text {
-			t.status.Text = txt
-			t.tp.Echo(txt)
+		if txt != task.status.Text {
+			task.status.Text = txt
+			task.tp.Echo(txt)
 		}
 	}()
 
 	if d == nil {
-		t.status.Active = false
-		txt = "(finished)"
+		task.status.Active = false
+		txt = "(Finished)"
 		return
 	}
 
 	// Keep a couple of seen replies.
-	t.status.LastCommand = d.LastSent
-	t.status.LastReply = d.Reply
+	task.status.LastCommand = d.LastSent
+	task.status.LastReply = d.Reply
 
-	if sz := t.gcodeStream.Size(); sz > 0 {
-		t.status.Done = float64(t.gcodeStream.Pos()) / float64(sz) * 100
+	if sz := task.gcodeStream.Size(); sz > 0 {
+		task.status.Done = float64(task.gcodeStream.Pos()) / float64(sz) * 100
 	}
 
 	// Assemble text and send it to printer on changes.
-	txt = fmt.Sprintf("%.1f%% (%s)", t.status.Done, t.gcodeStream.Name())
+	txt = fmt.Sprintf("%.1f%% (%s)", task.status.Done, task.gcodeStream.Name())
 }
