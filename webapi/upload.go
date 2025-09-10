7b9b70da8516d630151a7c6d783b2052389565d5
Axel Sepulveda <ansepulveda@uc.cl>
2025-09-10 19:20:21 -0300

    Caps

--- webapi/upload.go
+++ webapi/upload.go
@@ -21,20 +21,20 @@ type uploadReplyFiles struct {
 func (wapi *WebApi) localUpload(w http.ResponseWriter, rq *http.Request) {
 	f, h, err := rq.FormFile("file")
 	if err != nil {
-		wapi.error(w, "failed to parse form data")
+		wapi.error(w, "Failed to parse form data")
 		return
 	}
 
 	if h.Size > maxUploadSize {
-		wapi.error(w, "file upload exceeds limit")
+		wapi.error(w, "File upload exceeds limit")
 		return
 	}
-
+	// Optional path parameter
 	path := rq.FormValue("path")
-	wapi.log("Specified path: %s", path)
+	wapi.log("Specified save path: %s", path)
 
 	if err := wapi.storage.UploadFile(path, h.Filename, f); err != nil {
-		wapi.error(w, "failed to store file")
+		wapi.error(w, "Failed to store file")
 		return
 	}
 
@@ -59,11 +59,11 @@ func (wapi *WebApi) localUpload(w http.ResponseWriter, rq *http.Request) {
 		wapi.log("Enqueueing: %s for printing after upload. Shutdown after print: %v", h.Filename, shutdown)
 		instr, err := wapi.storage.ReadFile(path, h.Filename)
 		if err != nil {
-			wapi.log("failed to read file we just uploaded: %v", err)
+			wapi.log("Failed to read file we just uploaded: %v", err)
 			return
 		}
 		if err := wapi.enqueuePrint(instr, shutdown); err != nil {
-			wapi.log("enqueueing failed: %v", err)
+			wapi.log("Enqueueing failed: %v", err)
 			return
 		}
 	}
